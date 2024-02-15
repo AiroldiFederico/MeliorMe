b948836ec91e7416bf372c11c23cbb16d356535c
Federico Airoldi <airoldibusiness@gmail.com>
2024-02-15 22:58:16 +0100

    Progress component creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./style/App.css";
+import Progress from "./components/Progress.jsx";
 
 function App() {
   return (
-    <main className="bg-gray-950 w-screen h-screen text-slate-100 font-rubik flex justify-center ">
+    <main className="bg-zinc-950 w-screen h-screen text-slate-100 font-rubik flex justify-center ">
         <section className="w-[60rem]  h-full border p-8">
-            <p className="text-3xl font-bold">MeliorMe</p>
+            <h1 className="text-3xl font-bold">MeliorMe</h1>
+
+            <Progress></Progress>
         </section>
     </main>
   );
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,8 +1,33 @@
 
-
+import { useState } from 'react';
 
 export default function Progress() {
-	return <section>
-		test
+
+	// Genera l'array dei giorni dell'anno
+	const daysOfYear = Array.from({ length: 365 }, (_, index) => index + 1);
+
+	// Stato per tracciare il giorno selezionato
+	const [selectedDay, setSelectedDay] = useState(null);
+
+	// Funzione per gestire la selezione di un giorno
+	const handleDayClick = (day) => {
+		setSelectedDay(day);
+	};
+
+
+	return <section className="flex flex-col mt-9 flex-wrap gap-1 w-1 h-40" >
+
+		<h2></h2>
+
+		{daysOfYear.map(day => (
+			<div
+				key={day}
+				className={`day ${selectedDay === day ? 'selected' : ''} rounded bg-gray-900 p-2`}
+				onClick={() => handleDayClick(day)}
+			>
+				{}
+			</div>
+		))}
+
 	</section>
 }
